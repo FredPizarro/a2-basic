12732639d2d84e6f0e8a33e90be81d9adeb0ea6f
FredPizarro <freddy_ps_3@hotmail.com>
2016-10-26 10:42:37 -0500

    213.0_App.Bootstrap.Process.(AutomaticoCR6).&.AppModules.(NgModule)_(Angular CLI) (Skyrider Tuto)

--- First-APP/src/app/app.module.ts
+++ First-APP/src/app/app.module.ts
@@ -22,3 +22,18 @@ import { SearchComponent } from './components/navbar/search.component'; /* compo
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+/* 
+* .../index.html               entry point of your application.
+
+* .../inline.js                webpack loader
+* .../style.[hash].bundle.js   the style
+* .../main.[hash].bundle.js    
+                               app bundled with required (depending on your app)
+                               @angular/* dependencies declared in package.json,
+                               "RxJS" dependency and "polyfill" dependencies
+                               [ size: ~440 KB for new Angular CLI application empty,
+                                ~100 KB compressed. For a medium size application the
+                                compressed size is around 190 KB].
+* .../[hash].bundle.map        the SourceMaps corresponding to the previous files
+*/
--- First-APP/src/main.ts
+++ First-APP/src/main.ts
@@ -1,12 +1,23 @@
 import './polyfills.ts';
 
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic'; /* Necessities from @angular, and then call the bootstrap */
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
-import { AppModule } from './app/';
+import { AppModule } from './app/';      /* Finally, we need to import our exported AppModule */
 
 if (environment.production) {
   enableProdMode();
 }
-
+/*
+* Note that how we’ve used ‘platform-browser-dynamic’ to target the
+   browser platform.
+ */
 platformBrowserDynamic().bootstrapModule(AppModule);
+ /* 
+   platformBrowserDynamic() : function returns a few new methods on the
+                              prototype chain that we can invoke, the one
+                              we need is bootstrapModule
+   bootstrapModule function : we import gets invoked, and we pass in the
+                              AppComponent reference, of which is going to
+                               be our “Root component”
+ */
